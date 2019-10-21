3cfd73c5b0f156bf70b10338ebeab5b58373f41d
Naumov Konstantin <constantin.naumov@gmail.com>
2019-10-21 22:36:11 +0300

    Remove unused files

---
+++ app/Http/Controllers/AbstractBaseController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * Class AbstractBaseController
+ * @package App\Http\Controllers
+ */
+abstract class AbstractBaseController extends Controller
+{
+}
