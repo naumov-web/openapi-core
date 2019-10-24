d8f13ed80074c3dc17d75fc939a229d278cfbb44
Naumov Konstantin <naumov-web@yandex.ru>
2019-10-24 13:50:58 +0300

    Tests for registration

--- tests/Feature/Core/Auth/RegisterTest.php
+++ tests/Feature/Core/Auth/RegisterTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Core\Auth;
 
+use App\Models\User;
 use Illuminate\Http\Response;
 use Tests\Feature\BaseFeatureTest;
 
@@ -23,6 +24,47 @@ class RegisterTest extends BaseFeatureTest
         $data = [];
         $this->json('POST', route('core.auth.register'), $data)
             ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        $data['email'] = 'test@test.com';
+        $this->json('POST', route('core.auth.register'), $data)
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        $data['password'] = '123456';
+        $this->json('POST', route('core.auth.register'), $data)
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        $data['password_confirmation'] = 'qweasd';
+        $this->json('POST', route('core.auth.register'), $data)
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        $data['password_confirmation'] = $data['password'];
+        $this->json('POST', route('core.auth.register'), $data);
+        $this->json('POST', route('core.auth.register'), $data)
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /**
+     * Check API, when we are using correct data
+     *
+     * @test
+     * @return void
+     */
+    public function testSuccess() : void
+    {
+        $data = [
+            'email' => 'test@test.com',
+            'password' => 'qweasd',
+            'password_confirmation' => 'qweasd'
+        ];
+        $this->json('POST', route('core.auth.register'), $data)
+            ->assertOk();
+
+        $this->assertDatabaseHas(
+            (new User())->getTable(),
+            [
+                'email' => $data['email']
+            ]
+        );
     }
 
 }
