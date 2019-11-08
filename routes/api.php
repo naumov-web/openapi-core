a764207dd91f4818153ee4103df8fcd08672cf84
Naumov Konstantin <constantin.naumov@gmail.com>
2019-11-08 23:54:31 +0300

    Docs for user account requests

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,11 @@ Route::prefix('core')->group(function() {
         Route::post('/login', 'Core\Auth\LoginController@login')->name('core.auth.login');
         Route::post('/register', 'Core\Auth\RegisterController@register')->name('core.auth.register');
     });
+
+    Route::prefix('account')->group(function() {
+        Route::get('/user', 'Core\Account\UserController@show')->name('core.account.user.show');
+        Route::put('/user', 'Core\Account\UserController@update')->name('core.account.user.update');
+    });
 });
 
 /*****************************************************
