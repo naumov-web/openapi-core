541963ea19bc165164b7f7a09d5348750bcbf8f4
Naumov Konstantin <constantin.naumov@gmail.com>
2019-11-23 01:07:47 +0300

    Create projects table

---
+++ database/migrations/2019_11_22_212853_create_projects_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class CreateProjectsTable
+ */
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('owner_id');
+            $table->string('name', 500);
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('format_id');
+            $table->string('key', 21);
+            $table->timestamps();
+        });
+
+        Schema::table('projects', function (Blueprint $table) {
+            $table->foreign('owner_id')->references('id')->on('owners');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeign(['owner_id']);
+        });
+
+        Schema::dropIfExists('projects');
+    }
+}
